46a712325da84018569d8b5d1d8dc7c6f37620dc
wuyuanliang <wuyuanliang@Lenovo-PC>
2018-07-01 00:44:55 +0800

    初始commit

---
+++ Project8-ConfigServer/src/main/java/gcjs/custom/Project8ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package gcjs.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project8ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project8ConfigServerApplication.class, args);
+	}
+}
---
+++ Project8-ConfigServer/src/test/java/gcjs/custom/Project8ConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package gcjs.custom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Project8ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
